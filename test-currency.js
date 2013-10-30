9ee9cb03dc5dbf430b33cb98be7f5cac3ae8070a
cponeill <caseyoneill78@hotmail.com>
2013-10-30 10:21:35 -0700

    Update test-currency.js

--- test-currency.js
+++ test-currency.js
@@ -1,7 +1,10 @@
+// Path uses ./ to indicate that module exists within same directory as application script
 var currency = require('./currency');
 
+// Use currency modules bitcoinToUs function.
 console.log('50 Bitcoins equals this amount of US Dollars:');
 console.log(currency.bitcoinToUs(50));
 
+// User curreny modules USToBitcoin function.
 console.log('30 US dollars equals this amount of Bitcoins:');
 console.log(currency.USToBitcoin(30));
