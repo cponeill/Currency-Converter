e0f450e8a87acdf0ea151f6fea455bf26b1f87bb
cponeill <caseyoneill78@hotmail.com>
2013-10-30 10:19:57 -0700

    Update currency.js

--- currency.js
+++ currency.js
@@ -1,13 +1,16 @@
-var bitcoinValue = 205.0;
+var bitcoinValue = 205.0; // Or whatever the value is at the moment. I am working on a way to bring that data into this script in real-time. 
 
 function roundDecimalPlace(amount) {
   return Math.round(amount * 100) / 100;
 }
 
+
+// bitcoinToUs function is set in exports module so it can be used by code requiring this module.
 exports.bitcoinnToUs = function(bitcoin) {
   return roundDecimalPlace(bitcoin * bitcoinValue);
 }
 
+// USToBitcoin function is also set in exports module.
 exports.USToBitcoin = function(us) {
   return roundDecimalPlace(us / bitcoinValue);
 }
