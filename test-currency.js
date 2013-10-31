afc95ba3f2866c6a93e86d50f9167d51e37427f3
cponeill <caseyoneill78@hotmail.com>
2013-10-31 11:51:47 -0700

    Update test-currency.js

--- test-currency.js
+++ test-currency.js
@@ -8,3 +8,7 @@ console.log(currency.bitcoinToUs(50));
 // User curreny modules USToBitcoin function.
 console.log('30 US dollars equals this amount of Bitcoins:');
 console.log(currency.USToBitcoin(30));
+
+// User currency modules bitcoinToCanada function.
+console.log('100 Canadian Dollars equals this amount of Bitcoins');
+console.log(currency.canadianToBitcoin);
